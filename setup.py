3653dadb9227530898c045e2bf50b7eceb726b55
Jose Irigon de Irigon <jose.irigon@tu-dresden.de>
2018-02-11 12:19:09 +0100

    adding setup and monitor

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='libs',
+      version='1.0',
+      description='libraries used in pyLyRT',
+      author='Jose Irigon',
+      author_email='irigon@gmail.com',
+      packages=['libs'],
+     )
