88116513b928ec8b281dcdf46230ce218c8081c8
Jose Irigon de Irigon <jose.irigon@tu-dresden.de>
2018-02-12 17:54:00 +0100

    testing nose

--- libs/monitor.py
+++ libs/monitor.py
@@ -1,21 +1,28 @@
 from inotify_simple import INotify, flags
 import importlib
+import _thread
 import os
 import sys
 
 from libs import g
 
-DYN_LIB_DIR='runtime_lib'
+
+def start_monitor_thread(path_to_runtime_lib):
+   try:
+      _thread.start_new_thread( monitor, (path_to_runtime_lib, ) )
+   except:
+      print ("Error: unable to start thread")
 
 # Define a function for the thread
-def monitor(name, delay):
+def monitor(path_to_runtime_lib):
+   print(os.getcwd())
    inotify = INotify()
    watch_flags = flags.CREATE | flags.DELETE | flags.DELETE_SELF
-   wd = inotify.add_watch(DYN_LIB_DIR+'/', watch_flags)
+   wd = inotify.add_watch(path_to_runtime_lib + '/', watch_flags)
    while True:
       for event in inotify.read():
          if event.name.endswith('.py'):
-            module_name = DYN_LIB_DIR + '.' + os.path.splitext(event.name)[0]
+            module_name = path_to_runtime_lib + '.' + os.path.splitext(event.name)[0]
             for flag in flags.from_mask(event.mask):
                print('{}, Flag name: {}, Module Name: {}  '.format(event, flag.name, module_name))
             try:
@@ -29,3 +36,10 @@ def monitor(name, delay):
             if i is not None:
                 c = { x:y for x,y in i.__dict__.items() if not x.startswith('__') }
                 g.roles.update(c)
+
+
+
+
+if __name__ == '__main__':
+    monitor('../runtime_lib')
+
--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
 
-import _thread
 import time
-import sys
-import os
+from libs import g
+from libs.monitor import start_monitor_thread
 
-from libs import g 
-from libs import monitor
-
-def start_monitor_thread():
-   try:
-      _thread.start_new_thread( monitor.monitor, ("Thread-1", 2, ) )
-   except:
-      print ("Error: unable to start thread")
-            
-
-start_monitor_thread()
+start_monitor_thread('runtime_lib')
 
 while 1:
     time.sleep(1)
